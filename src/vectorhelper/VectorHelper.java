70db6e8a8c33535fe310b5707ae600f46a70f01d
user <user@user-PC>
2017-10-24 06:08:55 +0200

    l'ajout des commentraires pour la méthode min_max_tab

--- src/vectorhelper/VectorHelper.java
+++ src/vectorhelper/VectorHelper.java
@@ -85,6 +85,15 @@ public class VectorHelper {
                  return tableau;
 	 }	
 
+        
+/**
+	 * @param tableau : un tableau en entrée qui contient des entiers
+	 * @return cc'est un tableau de taille 2 ,il contient dans ça 1 ére case 
+         * le max et dans la 2 ème cas le min
+         * ex: si tableau ={10,57,-3} alors la méthode renvoit un tableau ={57,-3}
+	 */
+    
+        
           public int [] min_max_tab(int tableau[]){
         //cette methode renvoit dans un tableau de taille 2 
         //le max(dans la 1 ére case du tableau) et le min(dans la 2 éme case du tableau) 
